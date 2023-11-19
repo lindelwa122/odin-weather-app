eccaa686c0871ddc130cd85b2eda0cef7384c4f2
Lindelwa <lindelwa.codes@gmail.com>
2023-11-19 19:38:48 +0200

    Update how the data is processed

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const processData = ({ forecastday }) => {
-  return forecastday.map(({ date, day, hour }) => {
-    const data = {
-      condition: day.condition.text,
-      avgTempInC: day.avgtemp_c,
-      avgTempInF: day.avgtemp_f,
-      uv: day.uv,
-      maxWind: day.maxwind_kph,
-      hour: [],
-    };
-
-    hour.forEach((h) => {
-      data.hour.push({
-        tempInC: h.temp_c,
-        tempInF: h.temp_f,
+const processData = (current, { forecastday }) => {
+  return {
+    condition: current.condition.text,
+    icon: current.condition.icon,
+    forecast: forecastday.map(({ date, day, hour }) => {
+      const data = {
+        avgTempInC: day.avgtemp_c,
+        avgTempInF: day.avgtemp_f,
+        uv: day.uv,
+        maxWind: day.maxwind_kph,
+        hour: [],
+      };
+  
+      hour.forEach((h) => {
+        data.hour.push({
+          tempInC: h.temp_c,
+          tempInF: h.temp_f,
+        });
       });
-    });
-
-    return { data, date };
-  });
+  
+      return { data, date };
+    })
+  }
 };
 
 const fetchData = async (city) => {
@@ -25,7 +28,18 @@ const fetchData = async (city) => {
     `https://api.weatherapi.com/v1/forecast.json?key=e72994fb87454c6b9ca122701231211&q=${city}&days=4`
   );
 
-  const { forecast } = await response.json();
-  return processData(forecast);
+  const { current, forecast } = await response.json();
+
+  console.log(current)
+
+  return processData(current, forecast);
 }
 
+console.log(fetchData('pretoria'))
+
+document.querySelector('form').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const query = document.querySelector('input[type=search]').value.toLowerCase();
+  const data = await fetchData(query);
+  console.log(data);
+});
