356d3ea2935edbaf0215df5f6f441d26b3f19af3
Lindelwa <lindelwa.codes@gmail.com>
2023-11-19 17:19:59 +0200

    Fetch from API and process data

---
+++ index.js
@@ -0,0 +1,33 @@
+const processData = ({ forecastday }) => {
+  return forecastday.map(({ date, day, hour }) => {
+    const data = {
+      condition: day.condition.text,
+      avgTempInC: day.avgtemp_c,
+      avgTempInF: day.avgtemp_f,
+      uv: day.uv,
+      maxWind: day.maxwind_kph,
+      hour: [],
+    };
+
+    hour.forEach((h) => {
+      data.hour.push({
+        tempInC: h.temp_c,
+        tempInF: h.temp_f,
+      });
+    });
+
+    return { data, date };
+  });
+};
+
+(async () => {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/forecast.json?key=e72994fb87454c6b9ca122701231211&q=pretoria&days=4"
+  );
+
+  const { forecast } = await response.json();
+
+  console.log(forecast);
+
+  console.log(processData(forecast));
+})();
