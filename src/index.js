af327d83a9de0e66a1bcbea9ea3620316b9cd0c9
Lindelwa <lindelwa.codes@gmail.com>
2023-11-23 08:22:04 +0200

    Add normalize.css and styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import { domManager, router } from 'dom-wizard';
-import landingPage from './routes/landingPage';
+import 'normalize.css';
 import './css/style.css';
-import routes from './router';
-
-router.register(routes);
-domManager.create(landingPage);
